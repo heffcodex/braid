22f57e1f77dfe26b8798015ae2f60a30f3f3ae63
heffcodex <dev@heffcodex.pro>
2023-05-03 21:29:52 +0300

    tune

--- middleware/encryptcookie.go
+++ middleware/encryptcookie.go
@@ -5,10 +5,10 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/encryptcookie"
 )
 
-func EncryptCookieConfigDefault(key string) fiber.Handler {
-	return encryptcookie.New(encryptcookie.Config{
+func EncryptCookieConfigDefault(key string) encryptcookie.Config {
+	return encryptcookie.Config{
 		Key: key,
-	})
+	}
 }
 
 func EncryptCookie(config encryptcookie.Config) fiber.Handler {
